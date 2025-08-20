b27504da6a2dc8ee76ca40ed7d567e143209f4c1
NikosZoros3533 <nikos7kozani@gmail.com>
2025-08-20 13:49:19 +0300

    feat: remove BentoGrid and BentoCard components, replace with Card component in Main page for improved layout

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,61 +1,62 @@
 import React from "react";
-import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
-
+import { PlusCircle,Pencil,Search,RefreshCw } from "lucide-react";
 import {
-  BellIcon,
-  CalendarIcon,
-  FileTextIcon,
-  GlobeIcon,
-  InputIcon,
-} from "@radix-ui/react-icons";
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const features = [
   {
-    Icon: FileTextIcon,
+    Icon: PlusCircle,
     name: "Add",
     description: "Add new data to the system.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
   },
   {
-    Icon: InputIcon,
-    name: "Update",
-    description: "Update existing data in the system.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
+    Icon: Pencil,
+    name: "Edit",
+    description: "Edit existing data in the system.",
   },
   {
-    Icon: GlobeIcon,
+    Icon: Search,
     name: "Browse",
     description: "Browse through the data available in the system.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
   },
   {
-    Icon: CalendarIcon,
+    Icon: RefreshCw,
     name: "Sync",
-    description: "Sync spatial data with external sources and make inconsistent check.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
+    description:
+      "Sync spatial data with external sources and make inconsistent check.",
   },
-
 ];
 
 export default function Main() {
   return (
-    <div className="flex justify-center min-h-auto">
-    <BentoGrid className="lg:grid-rows-3 mt-12 max-w-4xl mx-4">
-      {features.map((feature) => (
-        <BentoCard key={feature.name} {...feature} />
-      ))}
-    </BentoGrid></div>
+    <div className="flex h-full items-start justify-center w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-3xl mt-20 mx-4 ">
+      
+       {features.map((feature) => (
+          <Card key={feature.name} className="w-full max-w-sm h-full flex flex-col justify-between hover:shadow-lg transition-shadow duration-300">
+            <CardHeader>
+              <CardTitle >
+                {feature.name}
+              </CardTitle>
+              <CardDescription>{feature.description}</CardDescription>
+            </CardHeader>
+            <CardFooter className="flex justify-center">
+              <Button
+                variant="outline" asChild><feature.Icon className="w-12 h-12 text-primary cursor-pointer"/></Button>
+            </CardFooter>
+          
+        </Card>
+       ))}
+      </div>
+    </div>
   );
 }
+
+
+//className="flex flex-wrap justify-center gap-4 p-4 w-full mt-40 border"
