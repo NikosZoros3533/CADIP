dcef14f32f32ade1d03ffea9bc92331e1407dfa3
NikosZoros3533 <nikos7kozani@gmail.com>
2025-08-28 12:24:32 +0300

    ....

--- src/components/theme-switcher.jsx
+++ src/components/theme-switcher.jsx
@@ -3,6 +3,6 @@ import { AnimatedThemeToggler } from "./magicui/animated-theme-toggler";
 export default function ThemeSwitch() {
   return (
    
-    <AnimatedThemeToggler />
+    <AnimatedThemeToggler className="cursor-pointer"/>
   );
 }
