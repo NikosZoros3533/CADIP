674e8afbee7468bede049a8a62b88ba1c1640bdf
NikosZoros3533 <nikos7kozani@gmail.com>
2025-11-08 21:28:56 +0200

    CREATE:vocabs/constans/enums

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -8,43 +8,45 @@ import {
   Search,
 } from "lucide-react";
 
-export const entities = [
+export const entities = Object.freeze([
   {
     id: 1,
     title: "Ancient Monuments",
     url: "/ancient-monuments",
+    fetchUrl: "ancient_monuments",
     icon: Landmark,
   },
   {
     id: 2,
     title: "Movable Antiquities",
     url: "/movable-antiquities",
+    fetchUrl: "movable_antiquities",
     icon: Amphora,
   },
   {
     id: 3,
     title: "Controlled Areas",
     url: "/controlled-areas",
+    fetchUrl: "controlled_areas",
     icon: ShieldCheck,
   },
   {
     id: 4,
     title: "Areas Under Temporary Requisition",
     url: "/areas-under-temp-requisition",
+    fetchUrl: "autr",
     icon: LandPlot,
   },
   {
     id: 5,
     title: "Surveyed Areas",
     url: "/surveyed-areas",
+    fetchUrl: "surveyed_areas",
     icon: ChartArea,
   },
-];
+]);
 
-
-
-
-export const actions = [
+export const actions = Object.freeze([
   {
     icon: PlusCircle,
     name: "Add",
@@ -70,4 +72,107 @@ export const actions = [
   //   description:
   //     "Sync spatial data with external sources and make inconsistent check.",
   // },
-];
+]);
+
+export const vocabs = Object.freeze([
+  {
+    name: "accessibility",
+    displayName: "Accessibility",
+    fetchUrl: "accessibilities",
+  },
+  {
+    name: "bibliographic_sources",
+    displayName: "Bibliographic Sources",
+    fetchUrl: "bibliographic_sources",
+  },
+  {
+    name: "",
+    displayName: "Building Materials",
+    fetchUrl: "building_materials",
+  },
+  {
+    name: "",
+    displayName: "Building Methods",
+    fetchUrl: "building_methods",
+  },
+  {
+    name: "",
+    displayName: "Certainty",
+    fetchUrl: "certainties",
+  },
+  {
+    name: "",
+    displayName: "Current Use For Ancient Monuments",
+    fetchUrl: "am_current_uses",
+  },
+  {
+    name: "",
+    displayName: "Integrity",
+    fetchUrl: "integrities",
+  },
+  {
+    name: "",
+    displayName: "Legal Status",
+    fetchUrl: "legal_statuses",
+  },
+  {
+    name: "",
+    displayName: "Legislation",
+    fetchUrl: "legislations",
+  },
+  {
+    name: "",
+    displayName: "Monument Category",
+    fetchUrl: "monument_categories",
+  },
+  {
+    name: "",
+    displayName: "Morphology",
+    fetchUrl: "morphologies",
+  },
+  {
+    name: "",
+    displayName: "Ornamentation",
+    fetchUrl: "ornamentations",
+  },
+  {
+    name: "",
+    displayName: "Ownership",
+    fetchUrl: "ownerships",
+  },
+  {
+    name: "",
+    displayName: "Present State",
+    fetchUrl: "present_states",
+  },
+  {
+    name: "",
+    displayName: "Protection",
+    fetchUrl: "protections",
+  },
+  {
+    name: "",
+    displayName: "Bodies Responsible for Protection",
+    fetchUrl: "protection_responsible_bodies",
+  },
+  {
+    name: "",
+    displayName: "Public Access",
+    fetchUrl: "public_accesses",
+  },
+  {
+    name: "",
+    displayName: "Schedule",
+    fetchUrl: "schedules",
+  },
+  {
+    name: "",
+    displayName: "Setting",
+    fetchUrl: "settings",
+  },
+  {
+    name: "",
+    displayName: "Toponym",
+    fetchUrl: "toponyms",
+  },
+]);
