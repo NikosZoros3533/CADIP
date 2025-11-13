060c7808c9334ffb17b485e35c86123863586867
NikosZoros3533 <nikos7kozani@gmail.com>
2025-11-13 17:01:46 +0200

    just checking

--- src/pages/AncientMonuments/Search.jsx
+++ src/pages/AncientMonuments/Search.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function AncientMonumentsSearch() {
   return (
-    <div className='text-5xl ml-3'>Ancient Monuments Search Page Just checking</div>
+    <div className='text-5xl ml-3'>Ancient Monuments Search Page</div>
   )
 }
