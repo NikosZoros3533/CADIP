07cd7824be14e0f1efdc5135cf131ab1152c727e
NikosZoros3533 <nikos7kozani@gmail.com>
2025-11-16 15:42:34 +0200

    Constructing the Base of Monument Form

--- src/components/ancientMonuments/form-component.jsx
+++ src/components/ancientMonuments/form-component.jsx
@@ -11,6 +11,7 @@ import {
   Field,
   FieldContent,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
   FieldLegend,
@@ -26,6 +27,8 @@ import MultiCheckbox from "../vocabs/MultiCheckbox";
 import { MapPin } from "lucide-react";
 import LocationTable from "../form-components/location-table";
 import ToponymTable from "../form-components/toponym-table";
+import * as z from "zod";
+import { formOptions, useForm } from "@tanstack/react-form";
 
 const vocAcc = [
   { id: "1", labelEn: "Fully accessible site" },
@@ -39,7 +42,41 @@ const locationData = [
   { district: "3", town: "1", quarter: "2" },
 ];
 
+const monumentSchema = z.object({
+  name: z.string().nonempty("Name is required"),
+  alternativeName: z.string().optional(),
+  analyticalDescription: z.string().optional(),
+  description: z.string().nonempty("Description is required"),
+  monumentNumber: z.string().optional(),
+  isRecordComplete: z.boolean(),
+});
+
+const defaultMonumentValues = {
+  name: "",
+  alternativeName: "",
+  analyticalDescription: "",
+  description: "",
+  monumentNumber: "",
+  isRecordComplete: false,
+};
+
+const formOpts = formOptions({
+  defaultValues: defaultMonumentValues,
+});
+
 export default function AMForm({ monument = null }) {
+  const form = useForm({
+    ...formOpts,
+    validators: {
+      onSubmit: monumentSchema,
+      onBlur: monumentSchema,
+      onChange: monumentSchema,
+    },
+    onSubmit: ({ value }) => {
+      console.log(value);
+    },
+  });
+
   return (
     <Card className="w-full max-w-7xl place-self-center p-4 md:p-6 lg:p-10 my-6 md:my-15">
       <CardHeader className="flex flex-col w-full justify-center items-center">
@@ -49,39 +86,164 @@ export default function AMForm({ monument = null }) {
             <CardDescription>Edit and Save this Monument info</CardDescription>
           </>
         ) : (
-          <CardTitle>New Monument</CardTitle>
+          <>
+            <CardTitle>New Monument</CardTitle>
+            <CardDescription>
+              Fill in the details to add a new Monument
+            </CardDescription>
+          </>
         )}
       </CardHeader>
       <CardContent className="place-self-start w-full max-w-full">
-        <form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            form.handleSubmit();
+          }}
+        >
           <FieldGroup>
             <FieldSet>
               <div className="flex flex-col md:flex-row w-full justify-between gap-4">
                 <FieldGroup className="w-full">
-                  <Field>
-                    <FieldLabel>Name</FieldLabel>
-                    <Input placeholder="Castle of Limassol" />
-                  </Field>
-                  <Field>
-                    <FieldLabel>Alternative Name</FieldLabel>
-                    <Textarea placeholder="Castle of Limassol" />
-                  </Field>
-                  <Field>
-                    <FieldLabel>Analytical Description</FieldLabel>
-                    <Textarea placeholder="Describe the monument" />
-                  </Field>
-                  <Field>
-                    <FieldLabel>Description</FieldLabel>
-                    <Textarea placeholder="Describe the monument" />
-                  </Field>
-                  <Field>
-                    <FieldLabel>Monument Number</FieldLabel>
-                    <Input placeholder="monument num" />
-                  </Field>
+                  <form.Field name="name">
+                    {(field) => {
+                      const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid;
+                      return (
+                        <>
+                          <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                          <Input
+                            id={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            aria-invalid={isInvalid}
+                            placeholder="Castle of Limassol"
+                            autoComplete="off"
+                          />
+                          {isInvalid && (
+                            <FieldError errors={field.state.meta.errors} />
+                          )}
+                        </>
+                      );
+                    }}
+                  </form.Field>
+                  <form.Field name="alternativeName">
+                    {(field) => {
+                      const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid;
+                      return (
+                        <>
+                          <FieldLabel htmlFor={field.name}>
+                            Alternative Name
+                          </FieldLabel>
+                          <Textarea
+                            id={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            placeholder="Castle of Limassol"
+                            aria-invalid={isInvalid}
+                          />
+                          {isInvalid && (
+                            <FieldError errors={field.state.meta.errors} />
+                          )}
+                        </>
+                      );
+                    }}
+                  </form.Field>
+                  <form.Field name="analyticalDescription">
+                    {(field) => {
+                      const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid;
+                      return (
+                        <>
+                          <FieldLabel htmlFor={field.name}>
+                            Analytical Description
+                          </FieldLabel>
+                          <Textarea
+                            id={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            aria-invalid={isInvalid}
+                            placeholder="Describe the monument"
+                          />
+                          {isInvalid && (
+                            <FieldError errors={field.state.meta.errors} />
+                          )}
+                        </>
+                      );
+                    }}
+                  </form.Field>
+                  <form.Field name="description">
+                    {(field) => {
+                      const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid;
+                      return (
+                        <>
+                          <FieldLabel htmlFor={field.name}>
+                            Description
+                          </FieldLabel>
+                          <Textarea
+                            id={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            aria-invalid={isInvalid}
+                            placeholder="Describe the monument"
+                          />
+                          {isInvalid && (
+                            <FieldError errors={field.state.meta.errors} />
+                          )}
+                        </>
+                      );
+                    }}
+                  </form.Field>
+                  <form.Field name="monumentNumber">
+                    {(field) => {
+                      const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid;
+                      return (
+                        <>
+                          <FieldLabel htmlFor={field.name}>
+                            Monument Number
+                          </FieldLabel>
+                          <Input
+                            id={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            aria-invalid={isInvalid}
+                            placeholder="monument num"
+                          />
+                          {isInvalid && (
+                            <FieldError errors={field.state.meta.errors} />
+                          )}
+                        </>
+                      );
+                    }}
+                  </form.Field>
                   <div className="flex justify-between w-full">
                     <div className="flex items-center gap-3">
-                      <Checkbox id="toggle" />
-                      <Label htmlFor="toggle">Record Complete</Label>
+                      <form.Field name="isRecordComplete">
+                        {(field) => {
+                          return (
+                            <>
+                              <Checkbox
+                                id={field.name}
+                                checked={field.state.value}
+                                onCheckedChange={(checked) =>
+                                  field.handleChange(checked === true)
+                                }
+                              />
+                              <Label htmlFor={field.name}>
+                                Record Complete
+                              </Label>
+                            </>
+                          );
+                        }}
+                      </form.Field>
                     </div>
                     <Button type="button">
                       View Map
@@ -93,50 +255,61 @@ export default function AMForm({ monument = null }) {
               </div>
             </FieldSet>
           </FieldGroup>
+          {/* <Tabs defaultValue="location" className="py-6 w-full">
+            <TabsList className="flex flex-wrap lg:flex-row gap-2 h-auto">
+              <TabsTrigger value="location">Location</TabsTrigger>
+              <TabsTrigger value="function">Use/Function</TabsTrigger>
+              <TabsTrigger value="manufacturing">Manufacturing</TabsTrigger>
+              <TabsTrigger value="condition">Physical Cond</TabsTrigger>
+              <TabsTrigger value="description">Description</TabsTrigger>
+              <TabsTrigger value="setting">Setting</TabsTrigger>
+              <TabsTrigger value="associations">Associations</TabsTrigger>
+              <TabsTrigger value="protection">
+                Protection/Legal Status
+              </TabsTrigger>
+              <TabsTrigger value="documentation">Documentation</TabsTrigger>
+              <TabsTrigger value="recordingDetails">
+                Recording Details
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="location" className="w-full lg:min-w-4xl">
+              <Card className="dark:drop-shadow-xl border-2">
+                <CardContent className="flex flex-col gap-8">
+                  <div className="flex flex-col sm:flex-row items-start justify-center sm:items-center gap-6">
+                    <FieldLabel>Accessibility</FieldLabel>
+                    <MultiCheckbox title="accessibility" vocab={vocAcc} />
+                  </div>
+                  <div className="flex flex-wrap flex-col md:flex-row md:justify-evenly gap-4 p-3">
+                    <LocationTable />
+                    <ToponymTable />
+                  </div>
+                  <Field orientation="horizontal">
+                    <FieldLabel>Analytical Description</FieldLabel>
+                    <Textarea
+                      placeholder="Describe the monument"
+                      className="w-full "
+                    />
+                  </Field>
+                </CardContent>
+                <CardFooter className="flex gap-2">
+                  <Button variant="secondary">Save changes</Button>
+                  <Button variant="outline">Reset</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+          </Tabs> */}
+          <Button type="submit" className="mt-6">
+            Submit
+          </Button>
+          <Button
+            type="button"
+            className="ml-2"
+            variant="outline"
+            onClick={() => form.reset()}
+          >
+            Reset All
+          </Button>
         </form>
-        <Tabs defaultValue="location" className="py-6 w-full">
-          <TabsList className="flex flex-wrap lg:flex-row gap-2 h-auto">
-            <TabsTrigger value="location">Location</TabsTrigger>
-            <TabsTrigger value="function">Use/Function</TabsTrigger>
-            <TabsTrigger value="manufacturing">Manufacturing</TabsTrigger>
-            <TabsTrigger value="condition">Physical Cond</TabsTrigger>
-            <TabsTrigger value="description">Description</TabsTrigger>
-            <TabsTrigger value="setting">Setting</TabsTrigger>
-            <TabsTrigger value="associations">Associations</TabsTrigger>
-            <TabsTrigger value="protection">
-              Protection/Legal Status
-            </TabsTrigger>
-            <TabsTrigger value="documentation">Documentation</TabsTrigger>
-            <TabsTrigger value="recordingDetails">
-              Recording Details
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="location" className="w-full lg:min-w-4xl">
-            <Card className="dark:drop-shadow-xl border-2">
-              <CardContent className="flex flex-col gap-8">
-                <div className="flex flex-col sm:flex-row items-start justify-center sm:items-center gap-6">
-                  <FieldLabel>Accessibility</FieldLabel>
-                  <MultiCheckbox title="accessibility" vocab={vocAcc} />
-                </div>
-                <div className="flex flex-wrap flex-col md:flex-row md:justify-evenly gap-4 p-3">
-                  <LocationTable />
-                  <ToponymTable />
-                </div>
-                <Field orientation="horizontal">
-                  <FieldLabel>Analytical Description</FieldLabel>
-                  <Textarea
-                    placeholder="Describe the monument"
-                    className="w-full "
-                  />
-                </Field>
-              </CardContent>
-              <CardFooter className="flex gap-2">
-                <Button variant="secondary">Save changes</Button>
-                <Button variant="outline">Reset</Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-        </Tabs>
       </CardContent>
     </Card>
   );
